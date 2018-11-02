25efc2e7eda1cb7bf01a9e3db8fb8e9cec45615e
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2018-11-02 20:41:18 +0100

    doOn* exercise

--- reactor/src/test/java/com/nurkiewicz/reactor/R035_DoOn.java
+++ reactor/src/test/java/com/nurkiewicz/reactor/R035_DoOn.java
@@ -7,6 +7,11 @@ import org.slf4j.LoggerFactory;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
+import java.math.BigDecimal;
+import java.util.concurrent.atomic.LongAdder;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @Ignore
 public class R035_DoOn {
 
@@ -78,5 +83,46 @@ public class R035_DoOn {
 				.doOnTerminate(() -> log.info("Terminated, reason unknown"))
 				.subscribe();
 	}
+
+	/**
+	 * TODO Parse each number using {@link #tryParse(String)}
+	 * <p>
+	 * Count how many input and output numbers you found and if there was an error
+	 * </p>
+	 */
+	@Test
+	public void countEvents() throws Exception {
+		//given
+		LongAdder inputCount = new LongAdder();
+		LongAdder outputCount = new LongAdder();
+		LongAdder errorCount = new LongAdder();
+
+		//when
+		Flux
+				.just("1.00", "2.500", "3", "meh")
+				.subscribe(
+						x -> log.info("Received {}", x),
+						e -> log.warn("Stack-trace intentionally left blank: {}", e.toString())
+				);
+		//TODO add operators here
+
+		//then
+		assertThat(inputCount.sum()).isEqualTo(4);
+		assertThat(outputCount.sum()).isEqualTo(5);
+		assertThat(errorCount.sum()).isEqualTo(1);
+	}
+
+	Flux<BigDecimal> tryParse(String s) {
+		return Flux.defer(() -> {
+			final BigDecimal std = new BigDecimal(s);
+			final BigDecimal stripped = std.stripTrailingZeros();
+			if (std.equals(stripped)) {
+				return Flux.just(std);
+			} else {
+				return Flux.just(std, stripped);
+			}
+		});
+	}
+
 
 }
