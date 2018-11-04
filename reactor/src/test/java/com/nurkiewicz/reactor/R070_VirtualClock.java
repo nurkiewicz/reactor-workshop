9622efd0fdcbf84dd7fd0ab51fe9afac74f983eb
Tomasz Nurkiewicz <tomasz.nurkiewicz@allegro.pl>
2018-11-04 18:46:33 +0200

    Virtual time

--- reactor/src/test/java/com/nurkiewicz/reactor/R070_VirtualClock.java
+++ reactor/src/test/java/com/nurkiewicz/reactor/R070_VirtualClock.java
@@ -4,16 +4,11 @@ import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
-import reactor.test.scheduler.VirtualTimeScheduler;
-import reactor.util.function.Tuple2;
-import reactor.util.function.Tuples;
 
 import java.time.Duration;
 
-import static java.time.Duration.*;
+import static java.time.Duration.ofSeconds;
 import static reactor.test.StepVerifier.withVirtualTime;
 
 @Ignore
@@ -42,22 +37,6 @@ public class R070_VirtualClock {
 		return Mono
 				.delay(Duration.ofMillis(2000))
 				.map(x -> "OK");
-	}
-
-	@Test
-	public void should() throws Exception {
-		//given
-		final VirtualTimeScheduler time = VirtualTimeScheduler.create();
-		final Flux<Long> slow = Flux.interval(ofDays(1), time);
-		final Flux<Long> fast = Flux.interval(ofHours(6), time);
-
-		//when
-		final Flux<Tuple2<Long, Long>> ticks = Flux.combineLatest(fast, slow, Tuples::of);
-
-		//then
-		final StepVerifier.FirstStep<Tuple2<Long, Long>> sv = StepVerifier.create(ticks);
-		time.advanceTimeBy(ofHours(6));
-		sv.expectNext(Tuples.of(0L, 0L));
 	}
 
 }
