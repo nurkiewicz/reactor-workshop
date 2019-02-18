d20e83a15dddd14518aa2af1c4da253685c214da
Tomasz Nurkiewicz <tomasz.nurkiewicz@allegro.pl>
2019-02-18 23:46:41 +0100

    Solutions to emoji tasks

--- webflux/src/main/java/com/nurkiewicz/webflux/demo/emojis/EmojiController.java
+++ webflux/src/main/java/com/nurkiewicz/webflux/demo/emojis/EmojiController.java
@@ -1,5 +1,6 @@
 package com.nurkiewicz.webflux.demo.emojis;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.codec.ServerSentEvent;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,7 +9,13 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
 import java.net.URI;
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
+import static java.util.Comparator.comparing;
+import static java.util.stream.Collectors.toMap;
 import static org.springframework.http.MediaType.TEXT_EVENT_STREAM_VALUE;
 
 @RestController
@@ -24,10 +31,7 @@ public class EmojiController {
 
     @GetMapping(value = "/emojis/raw", produces = TEXT_EVENT_STREAM_VALUE)
     Flux<ServerSentEvent> raw() {
-        return webClient
-                .get()
-                .uri(emojiTrackerUrl)
-                .retrieve()
+        return retrieve()
                 .bodyToFlux(ServerSentEvent.class);
     }
 
@@ -35,32 +39,64 @@ public class EmojiController {
      * TODO How many pushes from /subscribe/eps per second are emitted?
      */
     @GetMapping(value = "/emojis/rps", produces = TEXT_EVENT_STREAM_VALUE)
-    Flux<ServerSentEvent> rps() {
-        return Flux.empty();
+    Flux<Long> rps() {
+        return retrieve()
+                .bodyToFlux(ServerSentEvent.class)
+                .window(Duration.ofSeconds(1))
+                .flatMap(Flux::count);
     }
 
     /**
      * TODO How many emojis in total per second are emitted?
      */
     @GetMapping(value = "/emojis/eps", produces = TEXT_EVENT_STREAM_VALUE)
-    Flux<ServerSentEvent> eps() {
-        return Flux.empty();
+    Flux<Long> eps() {
+        return retrieve()
+                .bodyToFlux(Map.class)
+                .window(Duration.ofSeconds(1))
+                .flatMap(win -> win.reduce(0L, (c, data) -> c + sumValues(data)));
+    }
+
+    private long sumValues(Map data) {
+        return data
+                .values()
+                .stream()
+                .mapToInt(x -> (Integer) x)
+                .sum();
     }
 
     /**
      * TODO Total number of each emoji (ever-growing map)
      */
     @GetMapping(value = "/emojis/aggregated", produces = TEXT_EVENT_STREAM_VALUE)
-    Flux<ServerSentEvent> aggregated() {
-        return Flux.empty();
+    Flux<HashMap<String, Integer>> aggregated() {
+        return retrieve()
+                .bodyToFlux(Map.class)
+                .scan(new HashMap<>(), (HashMap<String, Integer> acc, Map data) -> {
+                    final HashMap<String, Integer> result = new HashMap<>(acc);
+                    data.forEach((k, v) -> {
+                        result.merge(k.toString(), (int) v, (old, n) -> old + n);
+                    });
+                    return result;
+                });
     }
 
     /**
      * TODO Top 10 most frequent emojis (with count)
      */
     @GetMapping(value = "/emojis/top10", produces = TEXT_EVENT_STREAM_VALUE)
-    Flux<ServerSentEvent> top10() {
-        return Flux.empty();
+    Flux<HashMap<String, Integer>> top10() {
+        return aggregated()
+                .map(this::top10values);
+    }
+
+    private HashMap<String, Integer> top10values(Map<String, Integer> agg) {
+        return new HashMap<>(agg
+                .entrySet()
+                .stream()
+                .sorted(comparing(e -> -e.getValue()))
+                .limit(10)
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue)));
     }
 
     /**
@@ -68,8 +104,25 @@ public class EmojiController {
      * @see #codeToEmoji(String)
      */
     @GetMapping(value = "/emojis/top10str", produces = TEXT_EVENT_STREAM_VALUE)
-    Flux<ServerSentEvent> top10str() {
-        return Flux.empty();
+    Flux<String> top10str() {
+        return top10()
+                .map(this::keysAsOneString);
+    }
+
+    private String keysAsOneString(HashMap<String, Integer> m) {
+        return m
+                .keySet()
+                .stream()
+                .map(EmojiController::codeToEmoji)
+                .collect(Collectors.joining());
+    }
+
+    @NotNull
+    private WebClient.ResponseSpec retrieve() {
+        return webClient
+                .get()
+                .uri(emojiTrackerUrl)
+                .retrieve();
     }
 
     private static String codeToEmoji(String hex) {
