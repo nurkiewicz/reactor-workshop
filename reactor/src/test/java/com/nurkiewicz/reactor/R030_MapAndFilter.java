d82d4e45b4b4920715a348d4e8e1dcdda17f130b
Tomasz Nurkiewicz <tomasz.nurkiewicz@allegro.pl>
2020-01-13 13:19:36 +0100

    Wrong test name

--- reactor/src/test/java/com/nurkiewicz/reactor/R030_MapAndFilter.java
+++ reactor/src/test/java/com/nurkiewicz/reactor/R030_MapAndFilter.java
@@ -70,7 +70,7 @@ public class R030_MapAndFilter {
 	 * TODO only pick words starting with 'e' and ending with 't'. But first remove comma or dot from word ending.
 	 */
 	@Test
-	public void lengthOfAllWordsEndingWithT() throws Exception {
+	public void allWordsStartingWithEAndEndingWithT() throws Exception {
 		//given
 		final Flux<String> words = Flux.just(LoremIpsum.words());
 
