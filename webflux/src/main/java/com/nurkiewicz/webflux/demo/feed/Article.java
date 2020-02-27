afa8538c8053db7e1b27f84209d518b15541d164
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2020-02-27 13:33:15 +0100

    WebFlux kickstart

---
+++ webflux/src/main/java/com/nurkiewicz/webflux/demo/feed/Article.java
@@ -0,0 +1,33 @@
+package com.nurkiewicz.webflux.demo.feed;
+
+import org.springframework.data.annotation.Id;
+
+import java.net.URI;
+import java.time.Instant;
+
+public class Article {
+
+    @Id
+    private final URI link;
+
+    private final Instant publishedDate;
+    private final String title;
+
+    public Article(URI link, Instant publishedDate, String title) {
+        this.link = link;
+        this.publishedDate = publishedDate;
+        this.title = title;
+    }
+
+    public URI getLink() {
+        return link;
+    }
+
+    public Instant getPublishedDate() {
+        return publishedDate;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+}
