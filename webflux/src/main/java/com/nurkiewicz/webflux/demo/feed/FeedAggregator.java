507101c3b38ecf60d729d1a3876c4c5ab2b760ea
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2020-02-27 13:34:55 +0100

    Remove TODOs from JavaDoc

--- webflux/src/main/java/com/nurkiewicz/webflux/demo/feed/FeedAggregator.java
+++ webflux/src/main/java/com/nurkiewicz/webflux/demo/feed/FeedAggregator.java
@@ -3,27 +3,13 @@ package com.nurkiewicz.webflux.demo.feed;
 import com.rometools.rome.io.FeedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.xml.sax.SAXException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
-import java.net.HttpURLConnection;
 import java.net.URL;
 
-/**
- * <ul>
- *	<li>Use {@link WebClient} instead of {@link HttpURLConnection}.</li>
- *	<li>Handle errors and 301 redirect</li>
- *	<li>Publish SSE stream of articles</li>
- *	<li>Polling for new articles periodically</li>
- *	<li>Store articles in the database (use e.g. {@link ReactiveMongoRepository})</li>
- *	<li>Create endpoints for browsing (e.g. most recent, about something, by author...)</li>
- *	<li>Make a simple front-end</li>
- * </ul>
- */
 @Component
 public class FeedAggregator {
 
