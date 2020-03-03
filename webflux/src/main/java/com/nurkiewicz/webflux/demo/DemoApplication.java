a42329ca4eab9ccfb5f10a9b87e6d0f34f01595c
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2020-03-03 19:44:57 +0100

    Configuration using embedded Docker

--- webflux/src/main/java/com/nurkiewicz/webflux/demo/DemoApplication.java
+++ webflux/src/main/java/com/nurkiewicz/webflux/demo/DemoApplication.java
@@ -3,15 +3,11 @@ package com.nurkiewicz.webflux.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.Duration;
-
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		InitDocker.start().block(Duration.ofSeconds(30));
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 
 }
--- webflux/src/main/java/com/nurkiewicz/webflux/demo/emojis/EmojiRepository.java
+++ webflux/src/main/java/com/nurkiewicz/webflux/demo/emojis/EmojiRepository.java
@@ -1,17 +1,10 @@
 package com.nurkiewicz.webflux.demo.emojis;
 
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
 @Component
 class EmojiRepository {
-
-    private final ReactiveRedisTemplate<String, String> redisTemplate;
-
-    public EmojiRepository(ReactiveRedisTemplate<String, String> redisTemplate) {
-        this.redisTemplate = redisTemplate;
-    }
 
     Mono<Long> inc(String emoji, long by) {
         return Mono.empty();
