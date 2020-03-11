9908d610c97d1fb8e5e3de5069a8736b86439076
Tomasz Nurkiewicz <nurkiewicz@gmail.com>
2020-03-11 09:46:22 +0100

    Solution to grouping Page hits

--- reactor/src/test/java/com/nurkiewicz/reactor/R074_GroupingStreams.java
+++ reactor/src/test/java/com/nurkiewicz/reactor/R074_GroupingStreams.java
@@ -1,12 +1,20 @@
 package com.nurkiewicz.reactor;
 
+import com.nurkiewicz.reactor.pagehit.Country;
+import com.nurkiewicz.reactor.pagehit.PageHit;
 import com.nurkiewicz.reactor.pagehit.PageHits;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.util.function.Tuple2;
+import reactor.util.function.Tuples;
 
-@Ignore
+import java.util.concurrent.TimeUnit;
+
+import static java.time.Duration.ofSeconds;
+
 public class R074_GroupingStreams {
 
     private static final Logger log = LoggerFactory.getLogger(R074_GroupingStreams.class);
@@ -16,6 +24,15 @@ public class R074_GroupingStreams {
      */
     @Test
     public void groupByCountryEverySecond() throws Exception {
+        PageHits
+                .random()
+                .window(ofSeconds(1))
+                .flatMap(oneSecond -> oneSecond
+                        .groupBy(PageHit::getCountry)
+                        .flatMap(byCountry -> count(byCountry.key(), byCountry))
+                )
+                .subscribe(System.out::println);
+        TimeUnit.SECONDS.sleep(10);
     }
 
     /**
@@ -23,6 +40,22 @@ public class R074_GroupingStreams {
      */
     @Test
     public void everySecondGroupByCountry() throws Exception {
+        PageHits
+                .random()
+                .groupBy(PageHit::getCountry)
+                .flatMap(byCountry -> byCountry
+                        .window(ofSeconds(1))
+                        .flatMap(countryInOnSecond ->
+                                count(byCountry.key(), countryInOnSecond)
+                        )
+                ).subscribe(System.out::println);
+        TimeUnit.SECONDS.sleep(10);
+    }
+
+    private Mono<Tuple2<Country, Long>> count(Country key, Flux<PageHit> countryInOnSecond) {
+        return countryInOnSecond
+                .count()
+                .map(cnt -> Tuples.of(key, cnt));
     }
 
 }
