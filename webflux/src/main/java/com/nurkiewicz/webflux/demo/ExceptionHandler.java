50860bddc22c166032e60e3afcfc8f2214f354ff
Tomasz Nurkiewicz <tnr@devskiller.com>
2021-06-28 09:14:38 +0200

    Upgrading Spring Boot 2.3.5 -> 2.5.2 Gradle 6.6.1 -> 7.1

--- webflux/src/main/java/com/nurkiewicz/webflux/demo/ExceptionHandler.java
+++ webflux/src/main/java/com/nurkiewicz/webflux/demo/ExceptionHandler.java
@@ -1,12 +1,14 @@
 package com.nurkiewicz.webflux.demo;
 
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.web.error.ErrorAttributeOptions;
 import org.springframework.boot.web.reactive.error.DefaultErrorAttributes;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.server.ServerRequest;
-
-import java.util.Map;
 
 @Component
 class ExceptionHandler extends DefaultErrorAttributes {
@@ -14,9 +16,10 @@ class ExceptionHandler extends DefaultErrorAttributes {
 	private static final Logger log = LoggerFactory.getLogger(ExceptionHandler.class);
 
 	@Override
-	public Map<String, Object> getErrorAttributes(ServerRequest request, boolean includeStackTrace) {
-		Map<String, Object> attributes = super.getErrorAttributes(request, includeStackTrace);
+	public Map<String, Object> getErrorAttributes(ServerRequest request, ErrorAttributeOptions options) {
+		Map<String, Object> attributes = super.getErrorAttributes(request, options);
 		log.error("Returning error to user: {}", attributes, getError(request));
 		return attributes;
 	}
+
 }
