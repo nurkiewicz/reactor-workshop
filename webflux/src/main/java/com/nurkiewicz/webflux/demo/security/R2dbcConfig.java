5343173fa41b6e592afaa2dccd8d11b148510239
Tomasz Nurkiewicz <tnr@devskiller.com>
2021-09-19 10:37:57 +0200

    Don't use deprecated R2DBC classes

--- webflux/src/main/java/com/nurkiewicz/webflux/demo/security/R2dbcConfig.java
+++ webflux/src/main/java/com/nurkiewicz/webflux/demo/security/R2dbcConfig.java
@@ -5,15 +5,15 @@ import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import org.springframework.data.r2dbc.connectionfactory.init.CompositeDatabasePopulator;
-import org.springframework.data.r2dbc.connectionfactory.init.ConnectionFactoryInitializer;
-import org.springframework.data.r2dbc.connectionfactory.init.ResourceDatabasePopulator;
+import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
+import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
+import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 @Configuration
 class R2dbcConfig {
 
 	@Bean
-	public ConnectionFactoryInitializer initializer(ConnectionFactory connectionFactory) {
+	public org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer  initializer(ConnectionFactory connectionFactory) {
 		ConnectionFactoryInitializer initializer = new ConnectionFactoryInitializer();
 		initializer.setConnectionFactory(connectionFactory);
 		initializer.setDatabasePopulator(new CompositeDatabasePopulator(
