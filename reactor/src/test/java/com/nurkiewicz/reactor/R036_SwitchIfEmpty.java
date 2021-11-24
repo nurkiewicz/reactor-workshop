171ffd9b503cceaeb65f475d159f2d8736d7e5c6
Tomasz Nurkiewicz <tnr@devskiller.com>
2021-11-24 12:13:21 +0100

    switchIfEmpty placeholder

---
+++ reactor/src/test/java/com/nurkiewicz/reactor/R036_SwitchIfEmpty.java
@@ -0,0 +1,12 @@
+package com.nurkiewicz.reactor;
+
+import org.junit.Ignore;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Ignore
+public class R036_SwitchIfEmpty {
+
+	private static final Logger log = LoggerFactory.getLogger(R036_SwitchIfEmpty.class);
+
+}
