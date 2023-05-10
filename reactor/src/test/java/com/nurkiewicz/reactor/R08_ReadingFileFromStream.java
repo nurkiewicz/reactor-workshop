0cfd6bf081e9541d656a4c98bad2c9f4f13dad45
Tomasz Nurkiewicz <tnr@devskiller.com>
2023-05-10 11:47:02 +0200

    Fixed assertion

--- reactor/src/test/java/com/nurkiewicz/reactor/R08_ReadingFileFromStream.java
+++ reactor/src/test/java/com/nurkiewicz/reactor/R08_ReadingFileFromStream.java
@@ -33,7 +33,7 @@ public class R08_ReadingFileFromStream {
 		final Long count = lines
 				.count()
 				.block();
-		assertThat(count).isEqualTo(12);
+		assertThat(count).isEqualTo(13);
 	}
 
 	private BufferedReader open(String path) {
